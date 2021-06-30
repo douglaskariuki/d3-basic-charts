facff51af43514f857e4cad510fb8741e68652f8
kariuki <kariukidouglas97@gmail.com>
2021-06-30 20:55:42 +0300

    with pie charts

---
+++ src/components/PieChart/BasicPieChart.test.ts
@@ -0,0 +1,15 @@
+import React from "react"
+import { shallow } from "enzyme"
+import BasicPieChart from "./BasicPieChart"
+
+describe("<BasicPieChart />", () => {
+    let component
+
+    beforeEach(() => {
+        component = shallow(<BasicPieChart width={900} height={400} top={10} right={50} bottom={50} left={50}/>)
+    })
+
+    test('It should mount', () => {
+        expect(component.length).toBe(1)
+    })
+})
