43eb8a1e3cce05fa800b7a63b831b606c3954577
kariuki <kariukidouglas97@gmail.com>
2021-07-17 14:58:14 +0300

    more charts... histogram charts

---
+++ src/components/HistogramCharts/Generalized-Histogram.tsx
@@ -0,0 +1,103 @@
+import React, { useEffect } from 'react';
+import * as d3 from "d3";
+
+export default function GeneralizedHistogram() {
+    useEffect(() => {
+        draw()
+    }, [])
+
+    async function draw() {
+        const dataset = await d3.json("/data/my_weather_data.json")
+
+        const width = 600
+
+        let dimensions = {
+            width,
+            height: width * 0.6,
+            margin: {
+                top: 60,
+                right: 10,
+                bottom: 50,
+                left: 50
+            },
+        }
+    
+        dimensions.boundedWidth = dimensions.width
+            - dimensions.margin.left
+            - dimensions.margin.right
+    
+        dimensions.boundedHeight = dimensions.height
+            - dimensions.margin.top
+            - dimensions.margin.bottom
+
+        const metrics = [
+            "windSpeed",
+            "moonPhase",
+            "dewPoint",
+            "humidity",
+            "uvIndex",
+            "windBearing",
+            "temperatureMin",
+            "temperatureMax",
+        ]
+
+        const drawHistogram = (metric) => {
+            const wrapper = d3.select(".wrapper")
+                .append("svg")
+                    .attr("width", dimensions.width)
+                    .attr("height", dimensions.height)
+
+            const metricAccessor = d => d[metric]
+            const yAccessor = d => d.length
+
+            const bounds = wrapper.append("g")
+                .style("transform", `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`)
+
+            const xScale = d3.scaleLinear()
+                .domain(d3.extent(dataset, metricAccessor))
+                .range([0, dimensions.boundedWidth])
+                .nice()
+
+            const binsGenerator = d3.histogram()
+                .domain(xScale.domain())
+                .value(metricAccessor)
+                .thresholds(12)
+
+            const bins = binsGenerator(dataset)
+
+            const yScale = d3.scaleLinear()
+                .domain([0, d3.max(bins, yAccessor)])
+                .range([dimensions.boundedHeight, 0])
+                .nice()
+
+            const binsGroup = bounds.append("g")
+
+            const binGroups = binsGroup.selectAll("g")
+                .data(bins)
+                .enter()
+                .append("g")
+
+            const barPadding = 1
+
+            const barRects = binGroups.append("rect")
+                .attr("x", d => xScale(d.x0) + barPadding / 2) // bar will start at the lower bound of the bin, xScale will convert it to humidity levels to pixel space
+                .attr("y", d => yScale(yAccessor(d)))
+                .attr("width", d => d3.max([
+                    0, xScale(d.x1) - xScale(d.x0) - barPadding
+                ]))
+                .attr("height", d => dimensions.boundedHeight
+                    - yScale(yAccessor(d))
+                )
+                .attr("fill", "cornflowerblue")
+            
+            
+        }
+
+        metrics.forEach(drawHistogram)
+    }
+    return (
+        <div className="wrapper">
+
+        </div>
+    )
+}
